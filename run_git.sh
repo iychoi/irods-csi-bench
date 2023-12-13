81c3589c3f359d185fdddbf0fcf38e87b33c5962
Illyoung Choi <iychoi@email.arizona.edu>
2023-12-13 14:25:32 -0700

    add run_git

---
+++ run_git.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+#
+# Run git.
+cfg=config.inc
+
+set -e
+
+
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+    readonly ExecName=$(greadlink -f "$0")
+else
+    readonly ExecName=$(readlink --canonicalize "$0")
+fi
+
+
+# escapes / and \ for sed script
+escape()
+{
+    local var="$*"
+
+    # Escape \ first to avoid escaping the escape character, i.e. avoid / -> \/ -> \\/
+    var="${var//\\/\\\\}"
+
+    printf '%s' "${var//\//\\/}"
+}
+
+
+expand_tmpl()
+{
+    cat <<EOF | sed --file - "$1"
+s/\$IRODSFS_PVC_NAME/$(escape $IRODSFS_PVC_NAME)/g
+s/\$IRODSFS_OVERLAYFS_PVC_NAME/$(escape $IRODSFS_OVERLAYFS_PVC_NAME)/g
+s/\$IRODSFS_FUSEOVERLAYFS_PVC_NAME/$(escape $IRODSFS_FUSEOVERLAYFS_PVC_NAME)/g
+
+s/\$GIT_NAME/$(escape $GIT_NAME)/g
+s/\$GIT_OVERLAYFS_NAME/$(escape $GIT_OVERLAYFS_NAME)/g
+s/\$GIT_FUSEOVERLAYFS_NAME/$(escape $GIT_FUSEOVERLAYFS_NAME)/g
+
+s/\$BENCHMARK_IMAGE/$(escape $BENCHMARK_IMAGE)/g
+EOF
+}
+
+
+run_wait_pod()
+{
+    expand_tmpl $1 | kubectl apply -f -
+    echo "wait for the pod to become ready"
+    kubectl wait --timeout=60s --for=condition=ready pod/$2
+    if [ $? -eq 0 ]
+    then
+        echo "wait for the pod to complete"
+        kubectl wait --timeout=-1s --for=condition=ready=False pod/$2
+        return 0
+    else
+        echo "wait timeout"
+        return -1
+    fi
+}
+
+main()
+{
+    local baseDir=$(dirname "$ExecName")
+  
+    . "$baseDir/$cfg"
+
+    # git
+    echo "running git with iRODS FUSE Lite"
+    run_wait_pod $baseDir/app/irodsfs_git.yaml.template $GIT_NAME
+    if [ $? -ne 0 ]
+    then
+        return -1
+    fi
+
+    echo "running git with iRODS FUSE Lite + OverlayFS"
+    run_wait_pod $baseDir/app/irodsfs_overlayfs_git.yaml.template $GIT_OVERLAYFS_NAME
+    if [ $? -ne 0 ]
+    then
+        return -1
+    fi
+    
+    echo "running git with iRODS FUSE Lite + Fuse-OverlayFS"
+    run_wait_pod $baseDir/app/irodsfs_fuseoverlayfs_git.yaml.template $GIT_FUSEOVERLAYFS_NAME
+    if [ $? -ne 0 ]
+    then
+        return -1
+    fi
+    
+    return 0
+}
+
+
+main "$@"
+
